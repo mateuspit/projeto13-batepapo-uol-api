31ef8ad0f963a507ebda24083ed3718875e0fbf7
Mateus Barcelos <mateuspit@hotmail.com>
2023-04-12 12:39:34 -0300

    first requiriment 'geral' done

--- src/app.js
+++ src/app.js
@@ -7,6 +7,24 @@ import apiPort from "../constants/apiPort.js";
 const server = express();
 server.use(cors());
 server.use(express.json());
+dotenv.config();
 
-server.listen(apiPort, ()=>console.log(`API running at port ${apiPort}`));
+// const mongoClient = new MongoClient("mongodb://localhost:27017/nomeDoBanco");
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db();
+    db.collection("contatos").find().toArray()
+        .then(contatos => {
+            console.log(contatos)
+        });
+}).catch((err) => console.log(err.message));
+
+// db.collection("contatos").find().toArray()
+//     .then(contatos => {
+//         console.log(contatos)
+//     });
+
+server.listen(apiPort, () => console.log(`API running at port ${apiPort}`));
 
