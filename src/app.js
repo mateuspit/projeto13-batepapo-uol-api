922542226f2f280f2f2e363e749dad4cea211b34
Mateus Barcelos <mateuspit@hotmail.com>
2023-04-12 12:46:42 -0300

    feature 'armazenamento e formato de dados' done

--- src/app.js
+++ src/app.js
@@ -10,11 +10,25 @@ server.use(express.json());
 dotenv.config();
 
 // const mongoClient = new MongoClient("mongodb://localhost:27017/nomeDoBanco");
-const mongoClient = new MongoClient(process.env.MONGO_URL);
-let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const db = mongoClient.db();
+// participante:
+// {
+//     name: 'João',
+//     lastStatus: 12313123
+// }
+
+//Mensagem:
+// {
+//     from: 'João',
+//     to: 'Todos', 
+//     text: 'oi galera', 
+//     type: 'message', 
+//     time: '20:04:37'
+// }
 
 mongoClient.connect().then(() => {
-    db = mongoClient.db();
+    
     db.collection("contatos").find().toArray()
         .then(contatos => {
             console.log(contatos)
